9fd6a3b28d33137c506957e0dc6bf59da543c5b6
issa-char <128964601+issa-char@users.noreply.github.com>
2023-10-06 10:38:03 -0400

    writing to file

--- fdandFile.c
+++ fdandFile.c
@@ -8,6 +8,8 @@
  * - Simplicity and ease of use
  * - Higher-level functions for reading and writing files
  * - Buffered I/O for efficient operations
+ * - <fprintf, fputs,fputc, fwrite>
+ * - modes: "w"-write, "r+"-read & write, "a"-append
  *
  * Approach 2: Using File Descriptors
  * - Lower-level control over file operations
@@ -15,13 +17,42 @@
  * - Suitable for specialized tasks and interacting with non-file resources.
  */
 
-/**
- * Approach one
- */
+// Approach one
 
 #include <stdio.h>
 
-FILE *ptr
+int main()
+{
+		FILE *fpt;  // declare FILE pointer for file handling
+
+		// if exist all existing contenti not deleted
+		// if not, it's created
+		fpt = fopen("mytext.txt", "a");
+
+		// check if file was opened successfully
+		if (fpt == NULL)
+		{
+				printf("error opening the file!");
+				return (1);
+		}
+
+		char a[] = "write";
+		char b[] = "read";
+		char c[] = "append";
+
+		fprintf(fpt, "\nhandling files: %s %s % s", a, b, c); // write formatted
+															  // data to file
+		fputs("\nusing FILE data type\n", fpt); // write string to file
+		putc('A', fpt); // write char
+
+		fclose(fpt); // close file when done
+		printf("data written successfully");
+
+
+		// reading file
+
+		return 0;
+}
 
 
 
