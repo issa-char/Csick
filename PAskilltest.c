787a7d3f035abae7ee09d1d344a1cef8f867531e
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-09 07:31:00 -0500

    looks ready

--- PAskilltest.c
+++ PAskilltest.c
@@ -2,7 +2,7 @@
 
 
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
 
 void reset_to_98(int *n)
 {
@@ -10,14 +10,32 @@ void reset_to_98(int *n)
 
 }
 
+void swap_int(int *a, int *b)
+{
+		// a =42 b =98
+		int d;
+		d = *a;
+		*a = *b;
+		*b = d;
+}
+
 int main(void)
 {
 		int n;
+		int a;
+		int b;
+		
+		a = 98;
+		b = 42;
 
 		n = 402;
 		printf("n=%d\n", n);
 		reset_to_98(&n);
 		printf("n=%d\n", n);
+		printf("a=%d, b=%d\n", a, b);
+		swap_int(&a, &b);
+		printf("a=%d, b=%d\n", a, b);
+
 		return(0);
 }
 
