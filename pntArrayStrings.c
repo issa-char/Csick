d655c3ec91a0f0baf862998170f94d8be40c2b60
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-09 00:50:55 -0500

    dereferencing

--- pntArrayStrings.c
+++ pntArrayStrings.c
@@ -15,15 +15,32 @@ int main(void)
 		int n;
 		char c;
 		float d;
+		int *z;
+		int *e = &n;
+		int t = 30;
+		int *v = &t;
 
-		printf("the size of 'char' on my machine is %lu\n", sizeof(char));
-		printf("the size of 'int' on my machine is %lu\n", sizeof(int));
-		printf("The size of 'float' on my machine is %lu\n", sizeof(float));
-		printf("the size of 'variable n' on my machine is %lu\n", sizeof(n));
+
+		printf("the size of 'char' on my machine is %luB\n", sizeof(char));
+		printf("the size of 'int' on my machine is %luB\n", sizeof(int));
+		printf("The size of 'float' on my machine is %luB\n", sizeof(float));
+		printf("the size of 'variable n' on my machine is %luB\n", sizeof(n));
+		printf("the size of 'pointer z' on my machine is %luB\n", sizeof(z));
 		printf("the mem addr of variable n is: %p\n", &n);
 		printf("the mem addr of variable c is: %p\n", &c);
 		printf("The mem addr of variable d is: %p\n", &d);
+		printf("the mem addr of pointer variable is %p\n", &z);
+		printf("the size of pointer e is %luB\n", sizeof(e));
+		printf("the mem add of e is %p\n", &e);
+		printf("The value of e is %p\n", e);
+		printf("the mem add of n stored in pointer e is %p\n", &n);
 
+		printf("the mem add of t is %lu\n", &t);
+		printf("The value of t is: %d\n", t);
+		printf("the value of pointer v is %lu\n", v);
+
+		*v = 888;
+		printf("the new value of t is %d\n", t);
 
 		return(0);
 }
