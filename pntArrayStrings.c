35dc4f2c9ecd10ca5e0ef7d72d6e34e264d55b11
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-09 00:20:08 -0500

    mem address

--- pntArrayStrings.c
+++ pntArrayStrings.c
@@ -13,11 +13,17 @@
 int main(void)
 {
 		int n;
+		char c;
+		float d;
 
 		printf("the size of 'char' on my machine is %lu\n", sizeof(char));
 		printf("the size of 'int' on my machine is %lu\n", sizeof(int));
 		printf("The size of 'float' on my machine is %lu\n", sizeof(float));
 		printf("the size of 'variable n' on my machine is %lu\n", sizeof(n));
+		printf("the mem addr of variable n is: %p\n", &n);
+		printf("the mem addr of variable c is: %p\n", &c);
+		printf("The mem addr of variable d is: %p\n", &d);
+
 
 		return(0);
 }
