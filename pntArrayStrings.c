e33b12283ff5733b2044b026e1bf80e94d464294
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-08 23:43:23 -0500

    sizes of types in c

--- pntArrayStrings.c
+++ pntArrayStrings.c
@@ -2,4 +2,23 @@
  * pointers arrays and strings
  * more pointers arrays and strings
  * even more pointers arrays and strings
+ * at the end  explain to anyone: what are pointers and how to use them
+ *  what are arrays and how to use them
+ *  what are the difference btwn pointers and arrays
+ *  how to use strings and how to manipulate them
+ *  scope of variables
  */
+#include <stdio.h>
+
+int main(void)
+{
+		int n;
+
+		printf("the size of 'char' on my machine is %lu\n", sizeof(char));
+		printf("the size of 'int' on my machine is %lu\n", sizeof(int));
+		printf("The size of 'float' on my machine is %lu\n", sizeof(float));
+		printf("the size of 'variable n' on my machine is %lu\n", sizeof(n));
+
+		return(0);
+}
+
