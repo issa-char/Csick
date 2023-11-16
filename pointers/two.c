92dadbc14b9e4edc7dce95b663f61b68f54127e7
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-16 05:19:04 -0500

    memory address of identifiers

---
+++ pointers/two.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+		char c;
+		int d;
+		float e;
+
+		printf("the memory addr of variable c in memory is %p\n", &c);
+		printf("the memory addr of variable d in memory is %p\n", &d);
+		printf("the memory addr of variable e in memory is %p\n", &e);
+
+		return(0);
+}
+
