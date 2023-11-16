36715ed1f2bd982e96a2cf0e344b0640c1c3c1eb
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-16 03:28:36 -0500

    read from file and write to file

--- fri.c
+++ fri.c
@@ -13,6 +13,8 @@ int main(void)
 		char *filename = "opentxt.txt";
 		char *content[40];
 
+		char text[30] = "reading and writting";
+
 		fd = open(filename, O_RDONLY, 0);
 		if (fd == -1)
 		{
@@ -28,6 +30,9 @@ int main(void)
 		bytes_read = read(fd, content, 20);	
 
 		printf("%s\n", content);
+
+		bytes_written = write(fd, text, 10);
+		printf("%d\n", bytes_written);
 		
 		close(fd);
 
