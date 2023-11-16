c4be9fc0385f7417e4e137fdd406d0b5c3ec4da2
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-16 05:50:46 -0500

    power of pointers

--- pointers/five.c
+++ pointers/five.c
@@ -2,31 +2,33 @@
 
 /**
  * modify_var - this function does not modify n
- * @n: a useless variable
+ * @n: a variable to change 
  *
  * Return: nothing
  */
-void modify_var(int n)
+void modify_var(int *n)
 {
-		n = 55;
+		*n = 55;
 
 }
 
 /**
- * main - parameter are passed by value
+ * main - parameter passed by a pointer 
  *
  * Return: Always 0
  */
 int main(void)
 {
 		int d;
+		int *pt;
 		int r;
 
 		d = 44;
 		r = 88;
+		pt = &d;
 		
 		printf("the value of d before modify function %d\n", d);
-		modify_var(d);
+		modify_var(pt);
 
 		printf("the value of d %d\n", d);
 
