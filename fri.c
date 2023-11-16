e7ab62f169a99aa694cf6475521443822f669395
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-16 02:23:42 -0500

    demonstrating low level file I0

---
+++ fri.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - demonstrating low level file I0
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+		int fd, c, t, h;
+		char *filename = "open.txt";
+
+		fd = open(filename, O_RDONLY, 0);
+
+		printf("%d\n", fd);
+
+
+		
+		close(fd);
+
+		return(0);
+}
+
