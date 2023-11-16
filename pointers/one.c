6fc28b478de2970ce483266f32fa3bbcd10d13ea
issa-char <128964601+issa-char@users.noreply.github.com>
2023-11-16 05:23:24 -0500

    pointer size in memory

--- pointers/one.c
+++ pointers/one.c
@@ -3,13 +3,14 @@
 int main(void)
 {
 		char arr[20];
+		int *ptr;
 
 		printf("the size of char in memory is %luB\n", sizeof(char));
 		printf("the size of int in memory is %luB\n", sizeof(int));
 		printf("the size of float in memory is %luB\n", sizeof(float));
 		printf("the size of double in memory is %luB\n", sizeof(double));
-		printf("the size of arra in memory is %luB\n", sizeof(&arr));
-
+		printf("the size of array in memory is %luB\n", sizeof(&arr));
+		printf("the size of a pointer variable in memory is %lu\n", sizeof(ptr));
 		return(0);
 }
 
